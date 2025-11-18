1f5d2a01cfc35a90bf6f6d8cdfc50760b4fdcddc
user <user@DESKTOP-KV9VBMJ>
2025-11-18 08:54:32 +0530

    add basic spring-boot project structure

---
+++ src/test/java/com/jokesapp/JokesApp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokesapp.JokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
